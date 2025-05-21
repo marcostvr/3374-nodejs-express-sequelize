a31dc6d3b66d69970c893e04d5d08e33c7a85efc
Marcos Vieira <m_tvieira@yahoo.com.br>
2025-05-21 13:21:10 -0300

    bug do pegaUmPorId não é um método resolvido

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,37 +1,71 @@
 class Controller {
-  constructor(entidadeService) {
-    this.entidadeService = entidadeService;
-  }
-  async pegaTodos(req, res) {
-    try {
-      const listaDeRegistros =
-        await this.entidadeService.pegaTodosOsRegistros();
-      return res.status(200).json(listaDeRegistros);
-    } catch (erro) {
-      //
+    constructor(entidadeService) {
+        this.entidadeService = entidadeService
     }
-  }
 
-  async atualiza(req, res) {
-    const { id } = req.params;
-	console.log(`id no atualiza do controller: ${id}`)
-    const dadosAtualizados = req.body;
-    try {
-      const foiAtualizado = await this.entidadeService.atualizaRegistro(
-        dadosAtualizados,
-        Number(id)
-      );
-      if (!foiAtualizado) {
-        return res.status(400).json({ mensagem: `id não foi atualizado` });
-      } else {
-        return res
-          .status(200)
-          .json({ mensagem: `Registro atualizado com sucesso!` });
-      }
-    } catch (erro) {
-      //
+    async pegaTodos(req, res) {
+        try {
+            const listaDeRegistro =
+                await this.entidadeService.pegaTodosOsRegistros()
+            return res.status(200).json(listaDeRegistro)
+        } catch (erro) {
+            // erro
+        }
     }
-  }
+
+    async pegaUmPorId(req, res) {
+        const { id } = req.params
+        try {
+            const umRegistro = await this.entidadeService.pegaUmRegistroPorId(
+                Number(id)
+            )
+            return res.status(200).json(umRegistro)
+        } catch (erro) {
+            // erro
+        }
+    }
+
+    async criaNovo(req, res) {
+        const dadosParaCriacao = req.body
+        try {
+            const novoRegistroCriado = await this.entidadeService.criaRegistro(
+                dadosParaCriacao
+            )
+            return res.status(200).json(novoRegistroCriado)
+        } catch (erro) {
+            // erro
+        }
+    }
+
+    async atualiza(req, res) {
+        const { id } = req.params
+        const dadosAtualizados = req.body
+        try {
+            //isUpdated
+            const foiAtualizado = await this.entidadeService.atualizaRegistro(
+                dadosAtualizados,
+                Number(id)
+            )
+            if (!foiAtualizado) {
+                return res
+                    .status(400)
+                    .json({ mensagem: "registro não foi atualizado" })
+            }
+            return res.status(200).json({ mensagem: "Atualizado com sucesso" })
+        } catch (erro) {
+            // erro
+        }
+    }
+
+    async exclui(req, res) {
+        const { id } = req.params
+        try {
+            await this.entidadeService.excluiRegistro(Number(id))
+            return res.status(200).json({ mensagem: `id ${id} deletado` })
+        } catch (error) {
+            // erro
+        }
+    }
 }
 
-module.exports = Controller;
+module.exports = Controller
